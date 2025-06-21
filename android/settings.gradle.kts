67f37248d161a9387760aa8ce3e2105d9e13511b
BTOPICT <biandaodilon@gmail.com>
2025-06-21 16:32:13 +0100

    version deja presentable

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,10 +18,10 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.2" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.android.library") version "8.9.2" apply false
+    id("com.android.library") version "8.10.1" apply false
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
